19ecf765e3c466e132a42aee1f96e04196ac716b
Steven <estev97@gmail.com>
2019-08-05 16:24:11 -0600

    Agregamos imprimir por consola

--- php/index.php
+++ php/index.php
@@ -19,7 +19,25 @@
       ,$card_type,$card_number,$cvc,$month,$year);
 
       echo "Hello, " . $h->getFname(). "! You are ";
+      jsLogs($h->getFname());
 
+   }
+
+   function jsLogs($data) {
+    $html = "";
+    $coll;
+
+    if (is_array($data) || is_object($data)) {
+        $coll = json_encode($data);
+    } else {
+        $coll = $data;
+    }
+
+    $html = "<script>console.log('PHP: ".$coll."');</script>";
+
+    echo($html);
+    # exit();
 }
+
 
 ?>
