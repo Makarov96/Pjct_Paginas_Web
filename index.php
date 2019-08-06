aef43f4a24b7dcc8855e2b39fcaf9ff24396e5cd
Steven <estev97@gmail.com>
2019-08-06 00:00:03 -0600

    Agregamos una actualizacion al formulario

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
- require "User.php";
+ require "user.php";
 
       $h = new User($_POST['first-name'] ,$_POST['last-name'],$_POST['address'],$_POST['apt'],$_POST['country']
       ,$_POST['card_name'],$_POST['card_number'],$_POST['cvc'],$_POST['expire-month'],$_POST['year']);
 
-      echo "Hello, " . $h->getFname(). "! You are ";
+      echo "Dataaaaaaaa" . $_POST['card_name'];
+      echo "Hello, " . $h->getFname() . "! your last name is: " . $h->getSname();
+      echo "<br>";
+      echo "Address: " . $h->getDirection() ;
+      echo "<br>";
+      echo "APT: " . $h->getApt();
+      echo "<br>";
+      echo "Country: " . $h->getCountry();
+      echo "<br>";
+      echo "Type Card: " . $h->getCardType();
+      echo "<br>";
+      echo "Card Number: " . $h->getCardNumber();
+      echo "<br>";
+      echo "CVC: " . $h->getCvc();
+      echo "<br>";
+      echo "Month: " . $h->getMonth();
+      echo "<br>";
+      echo "Year: " . $h->getYear();
 
 ?>
