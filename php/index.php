3074185620f08ac7d726848153233409ccf721a2
Steven <estev97@gmail.com>
2019-08-05 19:37:04 -0600

    Probando el metodo ge

--- php/index.php
+++ php/index.php
@@ -2,18 +2,18 @@
  require "User.php";
 
 
-    if ( filter_has_var( INPUT_POST, 'submit' ) ) {
+    if ( filter_has_var( INPUT_GET, 'submit' ) ) {
 
-      $first_name = $_POST['first-name'];
-      $last_name = $_POST['last-name'];
-      $direction= $_POST['address'];
-      $apt = $_POST['apt'];
-      $country = $_POST['country'];
-      $card_type = $_POST['card_name'];
-      $card_number = $_POST['last-name'];
-      $cvc = $_POST['cvc'];
-      $month = $_POST['expire-month'];
-      $year = $_POST['year'];
+      $first_name = $_GET['first-name'];
+      $last_name = $_GET['last-name'];
+      $direction= $_GET['address'];
+      $apt = $_GET['apt'];
+      $country = $_GET['country'];
+      $card_type = $_GET['card_name'];
+      $card_number = $_GET['last-name'];
+      $cvc = $_GET['cvc'];
+      $month = $_GET['expire-month'];
+      $year = $_GET['year'];
 
       $h = new User($first_name ,$last_name,$direction,$apt,$country
       ,$card_type,$card_number,$cvc,$month,$year);
